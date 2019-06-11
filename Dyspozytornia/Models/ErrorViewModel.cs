e7c17161f80cfc53f29d452491105d4a24b56636
Dawid Walenciak <divix55@icloud.com>
2019-06-11 21:29:22 +0200

    Change project type
    (cherry picked from commit abf713f8a47adacca5f24f0b9860e65770ebd56e)

---
+++ Dyspozytornia/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dyspozytornia.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
